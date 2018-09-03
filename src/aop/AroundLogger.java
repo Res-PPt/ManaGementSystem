e525604b176fa917f96eaf80d487dada87d7d8ea
ruier1 <948331956@qq.com>
2018-09-03 16:25:02 +0800

    h

--- src/aop/AroundLogger.java
+++ src/aop/AroundLogger.java
@@ -6,7 +6,7 @@ import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
- 
+  
 @Aspect  
 public class AroundLogger {
 	private static final Logger log = Logger.getLogger(AroundLogger.class);
